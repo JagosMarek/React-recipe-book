5a3533dd1fd2267215d520348cab34502e7ec2e2
JagosMarek <m.jagos@email.cz>
2023-10-18 19:42:10 +0200

    1. Blok

---
+++ cookbook/client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Marek Jagoš</h1>
+      <p></p>
+    </div>
+  );
+}
+
+export default App;
