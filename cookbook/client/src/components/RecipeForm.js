e77192aedd67d1ea0a8a313be73a7f131023ef30
JagosMarek <m.jagos@email.cz>
2023-11-08 18:30:16 +0100

    7. Blok - Uprava formulare + poslani dat na server

--- cookbook/client/src/components/RecipeForm.js
+++ cookbook/client/src/components/RecipeForm.js
@@ -3,16 +3,23 @@ import { Modal, Form, Button } from "react-bootstrap";
 import styles from "../css/modal.module.css";
 
 function RecipeForm(props) {
+  // State pro uchování dat formuláře.
   const [formData, setFormData] = useState({
     name: "",
     description: "",
-    ingredients: [{ name: "", amount: "", unit: "" }],
-    image: null,
+    ingredients: [{ id: "", amount: "", unit: "" }],
+    imgUri: "",
   });
 
+  // State pro validaci formuláře.
   const [validated, setValidated] = useState(false);
+  
+  // State pro zobrazení chyby při duplicitě ingrediencí.
   const [duplicateIngredientError, setDuplicateIngredientError] = useState(false);
+  
+  const unitOptions = ['ks', 'l', 'ml', 'g', 'kg', 'šálek', 'lžíce', 'lžička', 'špetka', 'kávová lžička', 'plátek', 'snítka', 'kávová lžička', 'párek', 'kousek'];
 
+  // Funkce pro aktualizaci hodnot ve state formData.
   const setField = (name, val) => {
     return setFormData((formData) => {
       const newData = { ...formData };
@@ -21,59 +28,92 @@ function RecipeForm(props) {
     });
   };
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    setField("image", file);
-  };
-
+  // Funkce pro odeslání formuláře.
   const handleSubmit = async (e) => {
     const form = e.currentTarget;
     e.preventDefault();
     e.stopPropagation();
 
+    // Kontrola na duplicitu ingrediencí.
     if (hasDuplicateIngredients()) {
       setDuplicateIngredientError(true);
       return;
     }
 
-    const formDataToSend = new FormData();
-    formDataToSend.append("name", formData.name);
-    formDataToSend.append("description", formData.description);
-    formDataToSend.append("image", formData.image);
+    // Příprava dat pro odeslání.
+    const formDataToSend = {
+      name: formData.name,
+      description: formData.description,
+      imgUri: formData.imgUri,
+      ingredients: formData.ingredients.map((ingredient) => ({
+        id: ingredient.id,
+        amount: parseFloat(ingredient.amount), 
+        unit: ingredient.unit,
+      })),
+    };
 
+    // Kontrola validace formuláře.
     if (!form.checkValidity()) {
       setValidated(true);
       return;
     }
 
-    console.log(formDataToSend);
-    handleClose();
+    // Asynchronní odeslání dat na server.
+    try {
+      const response = await fetch('http://localhost:3000/recipe/create', { 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formDataToSend),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result);
+      handleClose(); // Zavření modálního okna po úspěšném odeslání.
+    } catch (error) {
+      console.error('Chyba při odesílání formuláře:', error);
+    }
   };
 
+  // Funkce pro zavření modálního okna.
   const handleClose = () => props.setAddRecipeShow(false);
 
+  // Funkce pro kontrolu duplicitních ingrediencí podle ID.
   const hasDuplicateIngredients = () => {
-    const ingredientNames = formData.ingredients.map((ingredient) => ingredient.name);
-    return new Set(ingredientNames).size !== ingredientNames.length;
+    const ingredientIds = formData.ingredients.map((ingredient) => ingredient.id);
+    return new Set(ingredientIds).size !== ingredientIds.length;
   };
 
+  // Funkce pro manipulaci s ingrediencemi v state.
   const handleIngredientChange = (index, field, value) => {
-    setFormData((prevData) => {
+    if (duplicateIngredientError) {
+      setDuplicateIngredientError(false);
+    }
+  
+  // Funkce pro aktualizaci hodnoty konkrétní ingredience v poli ingrediencí.
+  setFormData((prevData) => {
       const newData = { ...prevData };
       newData.ingredients[index][field] = value;
       return newData;
     });
   };
 
+  // Funkce pro přidání nové ingredience do formuláře.
   const handleAddIngredient = () => {
-    setDuplicateIngredientError(false); // Reset duplicate ingredient error
+    setDuplicateIngredientError(false);
     setFormData((prevData) => {
       const newData = { ...prevData };
-      newData.ingredients = [...newData.ingredients, { name: "", amount: "", unit: "" }];
+      newData.ingredients = [...newData.ingredients, { id: "", amount: "", unit: "" }];
       return newData;
     });
   };
 
+  // Funkce pro odstranění ingredience z formuláře.
   const handleRemoveIngredient = (index) => {
     setFormData((prevData) => {
       const newData = { ...prevData };
@@ -116,10 +156,15 @@ function RecipeForm(props) {
               </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Label>{"Obrázek"}</Form.Label>
-              <input type="file" accept="image/*" onChange={handleImageChange} required />
+              <Form.Label>{"Obrázek URL"}</Form.Label>
+              <Form.Control
+                type="text"
+                value={formData.imgUri}
+                onChange={(e) => setField("imgUri", e.target.value)}
+                required
+              />
               <Form.Control.Feedback type="invalid">
-                Zadejte obrázek.
+                Zadejte URL obrázku.
               </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3">
@@ -127,42 +172,43 @@ function RecipeForm(props) {
               {formData.ingredients.map((ingredient, index) => (
                 <div key={`ingredient-${index}`} className="d-flex mb-2">
                   <Form.Select
-                    value={ingredient.name}
-                    onChange={(e) =>
-                      handleIngredientChange(index, "name", e.target.value)
-                    }
+                    value={ingredient.id}
+                    onChange={(e) => handleIngredientChange(index, "id", e.target.value)}
                     required
                   >
                     <option value="" disabled>
-                      {"Ingredience"}
+                      {"Vyberte ingredienci"}
                     </option>
-                    {props.ingredientList.map((ing, i) => (
-                      <option key={i} value={ing.name}>
-                        {ing.name}
-                      </option>
-                    ))}
+                    {[...props.ingredientList] // Vytvořím kopii pole, abych mohl bezpečně seřadit
+                      .sort((a, b) => a.name.localeCompare(b.name)) // Seřadím abecedně
+                      .map((ing, i) => ( // mapuji do <option>
+                        <option key={i} value={ing.id}>
+                          {ing.name}
+                        </option>
+                      ))
+                    }
                   </Form.Select>
                   <Form.Control
                     type="number"
                     placeholder="Počet"
                     value={ingredient.amount}
-                    onChange={(e) =>
-                      handleIngredientChange(index, "amount", e.target.value)
-                    }
-                    min={1}
-                    max={1000}
+                    onChange={(e) => handleIngredientChange(index, "amount", e.target.value)}
+                    step="0.1"
+                    min={0.1}
+                    max={10000}
                     required
                   />
-                  <Form.Control
-                    type="text"
-                    placeholder="Jednotka"
+                  <Form.Select
                     value={ingredient.unit}
-                    onChange={(e) =>
-                      handleIngredientChange(index, "unit", e.target.value)
-                    }
+                    onChange={(e) => handleIngredientChange(index, "unit", e.target.value)}
                     required
-                  />
-                  {index > 0 && ( // Zobrazit tlačítko "Odstranit" pouze pokud existuje více než jeden řádek
+                  >
+                    <option value="">Vyberte jednotku</option>
+                    {unitOptions.map((unit, i) => (
+                      <option key={i} value={unit}>{unit}</option>
+                    ))}
+                  </Form.Select>
+                  {index > 0 && (
                     <Button
                       variant="danger"
                       onClick={() => handleRemoveIngredient(index)}
